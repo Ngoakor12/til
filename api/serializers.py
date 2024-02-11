60b13034b2194f7b9bd5295d7c93e01789cd7fae
ngoakor12 <ngoakor12@gmail.com>
2024-02-11 22:08:09 +0200

    basic drf api working

---
+++ api/serializers.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import Group, User
+from rest_framework import serializers
+from .models import Topic, Post
+
+
+class TopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = "__all__"
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__"
---
+++ api/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.http import Http404
+from django.contrib.auth.models import Group, User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Topic, Post
+from .serializers import TopicSerializer, PostSerializer
+
+# Create your views here.
+
+
+class ListTopics(APIView):
+    def get(self, request):
+        topics = Topic.objects.all()
+        serializer = TopicSerializer(topics, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TopicSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TopicDetail(APIView):
+    """
+    Retrieve, update or delete a topic instance.
+    """
+
+    def get_object(self, pk):
+        try:
+            return Topic.objects.get(pk=pk)
+        except Topic.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        topic = self.get_object(pk)
+        serializer = TopicSerializer(topic)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        topic = self.get_object(pk)
+        serializer = TopicSerializer(topic, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        topic = self.get_object(pk)
+        topic.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
