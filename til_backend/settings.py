45ec41d61a6c318758075f1a42093aa0075bddeb
ngoakor12 <ngoakor12@gmail.com>
2024-03-23 23:50:06 +0200

    override auth user model

--- til_backend/settings.py
+++ til_backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
+AUTH_USER_MODEL = "api.User"
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -96,6 +98,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
